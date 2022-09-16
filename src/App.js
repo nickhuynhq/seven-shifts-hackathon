abbc07c2fe23d1c4b264c205d3d4050460e85ce8
Li Yu <gachuka@gmail.com>
2022-09-16 10:01:10 -0400

    Did Test

--- src/App.js
+++ src/App.js
@@ -2,22 +2,11 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <span className='test1'>Test Text</span>
+      <span className='test2'>Test Text</span>
+      <span className='test3'>Test Text</span>
+    </>
   );
 }
 
