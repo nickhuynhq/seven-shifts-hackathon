c6ced338f26a3b0568c5050e7e647453fb3767b7
Li Yu <gachuka@gmail.com>
2022-09-16 13:16:09 -0400

    Styled

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -5,39 +5,40 @@ import employee2 from '../../assets/images/employee2.png';
 import employee3 from '../../assets/images/employee3.png';
 import employee4 from '../../assets/images/employee4.png';
 import employee5 from '../../assets/images/employee5.png';
+import downArrow from '../../assets/icons/png/vector.png';
 
 function DashboardPage() {
 
   const staffList = [
     {
       request_icon: employee1,
-      request_type: "",
-      request_time: "",
-      request_description: ""
+      request_employee: "Sarah Martin",
+      request_time: "9:05 AM",
+      request_description: "is requestion a skillbuilding session."
     },
     {
       request_icon: employee2,
-      request_type: "",
-      request_time: "",
-      request_description: ""
+      request_employee: "Jack Nicola",
+      request_time: "10:24 AM",
+      request_description: "is requestion you to rview their availability schedule."
     },
     {
       request_icon: employee3,
-      request_type: "",
-      request_time: "",
-      request_description: ""
+      request_employee: "James Tank",
+      request_time: "10:50 AM",
+      request_description: "is requestion a shift change."
     },
     {
       request_icon: employee4,
-      request_type: "",
-      request_time: "",
-      request_description: ""
+      request_employee: "Naz Tayee",
+      request_time: "12:55 PM",
+      request_description: "is requesting you to review their available schedule"
     },
     {
       request_icon: employee5,
-      request_type: "",
-      request_time: "",
-      request_description: ""
+      request_employee: "Daniel Smith",
+      request_time: "2:10 PM",
+      request_description: "is notifying you of their sick leave"
     },
   ]
 
@@ -55,11 +56,27 @@ function DashboardPage() {
           </div>
         </div>
         <div className='requests__container'>
-          <h1 className='requests__header h1'>Staff Requests</h1>
+          <div className='requests__header'>
+            <h1 className='requests__title h1'>Staff Requests</h1>
+            <div className='requests__selection'>
+              <span className='requests__selection-option'>Show: Skillbuilding</span>
+              <img src={downArrow} alt='arrow' />
+            </div>
+          </div>
           <div className='requests__cards cards'>
             {staffList.map((staff) => {
               return (
-                <img className='cards__icon' src={staff.request_icon} alt='staff image' />
+                <div className='cards__container'>
+                  <img className='cards__icon' src={staff.request_icon} alt='staff image' />
+                  <div className='cards__info'>
+                    <div className='cards__who-when'>
+                      <div className='cards__who'>{staff.request_employee}</div>
+                      <div className='cards__when'>{staff.request_time}</div>
+                    </div>
+                    <div className='cards__description'>{staff.request_description}</div>
+                  </div>
+                  <div className='cards__button btn'>Show Request</div>
+                </div>
               )
             })}
           </div>
