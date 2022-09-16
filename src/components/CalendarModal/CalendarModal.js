3fbeb22a229917ff3cb03fa53a6385236a5e38c8
Nicholas Huynh <nickhuynhq@gmail.com>
2022-09-16 11:36:19 -0400

    Create Caldenar Modal Files

---
+++ src/components/CalendarModal/CalendarModal.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const CalendarModal = () => {
+  return (
+    <div className="caldendar-modal">
+
+        <div className="calendar-modal__employee-card">
+          <img/>
+          <h2>Cody Fisher</h2>
+          
+
+        </div>
+        CalendarModal
+    </div>
+  )
+}
+
+export default CalendarModal
