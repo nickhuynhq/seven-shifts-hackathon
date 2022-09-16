c1222c41ac62e0e5e9c364b7ffc12617fc0dd4b8
Li Yu <gachuka@gmail.com>
2022-09-16 11:36:16 -0400

    Base dashboard layout

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,10 +1,16 @@
 import './DashboardPage.scss';
+import dashboardGraph from '../../assets/images/dashboard_graph.png';
 
 function DashboardPage() {
   return (
-    <>
-    
-    </>
+    <section className='dashboard'>
+      <div className='dashboard__container'>
+        <h1></h1>
+        <div className='dashboard__static'>
+          <img className='dashboard__graph' src={dashboardGraph} alt='graph'/>
+        </div>
+      </div>
+    </section>
   )
 }
 
