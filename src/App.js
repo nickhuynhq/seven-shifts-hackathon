cd075d5bbcee349b08ae0e21110f9040da49b029
Li Yu <gachuka@gmail.com>
2022-09-16 11:47:30 -0400

    Made changes to navbar

--- src/App.js
+++ src/App.js
@@ -11,9 +11,6 @@ function App() {
         <Route path='/' element={<Navigate to="/dashboard" element={<DashboardPage />} />} />
         <Route path='/dashboard' element={<DashboardPage />} />
       </Routes>
-      <span className='test1'>Test Text</span>
-      <span className='test2'>Test Text</span>
-      <span className='test3'>Test Text</span>
     </BrowserRouter>
   );
 }
